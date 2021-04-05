a54745110c24dbbf250847e6d3fbae4c8028f73d
Pedro Manuel <pmmarquez@xetid.cu>
2021-04-05 19:30:30 +0000

    [ADD] stripe account fields [ADD] delete stripe account method

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from odoo.http import Response
 from odoo import api, fields, models, _
 import werkzeug.wsgi
 class Partner(models.Model):
@@ -19,11 +20,18 @@ class Partner(models.Model):
     social_security = fields.Char('Social security number')
     iae_code = fields.Char('I.A.E code')
     dni = fields.Char('DNI number')
-
+    
     product_supply_ids = fields.One2many('product.supplierinfo', 'name')
-
+    
     docs_check = fields.Boolean(default=True) #true when partner needs docs check
 
+    stripe_connect_account_id = fields.Char('Stripe connect account')
+    stripe_connect_account_state = fields.Selection(selection=[
+            ('false', 'False'),
+            ('created', 'Created'),
+            ('verified', 'Verified')
+        ], string='Stripe connect account state', default='false')
+    
     def set_docs_check(self):
         users = self.env['res.users'].search([('classification', '=', 'vendor')])
         for user in users:
@@ -39,25 +47,23 @@ class Partner(models.Model):
             'email': self.email,
         }
         account = payment_stripe._stripe_request('accounts', s2s_data_account)
-
         # create account link
         link = self.stripe_connect_account_link(account.get('id'), reauth_url, return_url)
-
         return_data = {
             'partner_id': self.id,
             'account_id': account.get('id'),
             'link': link.get('url'),
         }
-        
         # return link 
         if account.get('id') and link:
+            self.stripe_connect_account_id = account.get('id')
+            self.stripe_connect_account_state = 'created'
             return return_data
         else:
             return False
     
     def stripe_connect_account_link(self, account_id, reauth_url, return_url):
         payment_stripe = self.env['payment.acquirer'].search([('provider', '=', 'stripe')])
-
         # create account link
         s2s_data_account_link = {
             'account': account_id,
@@ -66,9 +72,19 @@ class Partner(models.Model):
             'type':'account_onboarding'
         }
         link = payment_stripe._stripe_request('account_links', s2s_data_account_link)
-        
         # return link 
         if account_id and link.get('url'):
             return link
+        else:
+            return False
+    
+    def delete_stripe_connect_account(self):
+        payment_stripe = self.env['payment.acquirer'].search([('provider', '=', 'stripe')])
+        response = payment_stripe._stripe_request('accounts/%s' % self.stripe_connect_account_id, data=False, method='DELETE')
+        # return link 
+        if response.get('deleted'):
+            self.stripe_connect_account_id = ''
+            self.stripe_connect_account_state = 'false'
+            return response
         else:
             return False
