3a3757763cc1b6d15144158c9e4ab992b079a4be
Pedro Manuel <pmmarquez@xetid.cu>
2021-04-11 04:30:27 +0000

    [FIX] mixed redirect and reauth controllers

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -19,7 +19,7 @@ class StripeLinkController(http.Controller):
             return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>Link gneration error...</title><p>Try manual: <a href="%s">%s</a>. click the link.' %(old_link, old_link)
         
     @http.route('/payment/stripe/return', type='http', auth='none')
-    def stripe_refresh_account_link(self,**kwargs):
+    def stripe_validate_account_link(self,**kwargs):
         data = kwargs.copy()
         old_link = request.env['res.partner'].sudo().search([('id', '=', data['partner_id'])]).stripe_connect_account_link
         account_verified = request.env['res.partner'].sudo().search([('id', '=', data['partner_id'])]).verify_stripe_connect_account()
@@ -31,11 +31,11 @@ class StripeLinkController(http.Controller):
             '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">\n'
             '<title>Redirecting...</title>\n'
             '<body><script>alert("Account data incomplete, redirect to account form");</script>\n'
-            '<h1>Redirecting...</h1>\n'
-            '<p>You should be redirected automatically to new link: '
-            '<a href="%s">%s</a>.  If not click the link.</p></body>' %
+            '<h1>Use link to redirect...</h1>\n'
+            '<p>Link: '
+            '<a href="%s">%s</a></p></body>' %
             (werkzeug.utils.escape(location), display_location), 302, mimetype='text/html')
-        error_response.headers['Location'] = location
+        #error_response.headers['Location'] = location
 
         if (account_verified):
             return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><title>Success...</title><h1>Correctly account verification</h1>'
